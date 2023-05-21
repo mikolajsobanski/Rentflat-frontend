035d911932a48a9992eca39b8a0d7446bd9b18aa
Mikolaj Sobanski <mikolajsobanski01@gmail.com>
2023-05-21 18:10:54 +0200

    RF-1 login update

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,26 +1,52 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom'
 import { Form, Button, Row, Col} from 'react-bootstrap'
 import FormContainer from '../components/FormContainer'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+import { login } from '../actions/userActions'
 
+function LoginScreen({location, history}) {
 
-function LoginScreen({location}) {
-
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    
+    const dispatch = useDispatch()
+    let reload = useNavigate()
     const redirect = location ? location.search.split('=')[1] : '/'
+
+    const userLogin = useSelector(state => state.userLogin)
+    const { error, loading, userInfo } = userLogin
+
+    useEffect(() => {
+        if (userInfo) {
+            history.push(redirect)
+        }
+    }, [history, userInfo, redirect])
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        dispatch(login(email, password))
+        reload('/')
+    }
     
     return (
         <div>
         
        <FormContainer>
             <h1 className='text-center py-4'>Sign In</h1>
-            
-            <Form >
+            {error && <Message variant='danger'>{error}</Message>}
+            {loading && <Loader />}
+            <Form onSubmit={submitHandler}>
                 <Form.Group controlId='email'>
                     <Form.Label>Email Address</Form.Label>
                     <Form.Control
                         type='email'
                         placeholder='Enter Email'
-                        
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
                     >
 
                     </Form.Control>
@@ -31,7 +57,8 @@ function LoginScreen({location}) {
                     <Form.Control
                         type='password'
                         placeholder='Enter password'
-                        
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
                     >
 
                     </Form.Control>
