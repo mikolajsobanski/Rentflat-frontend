4671749f0198fd9b602658b997e5551bf56b4a04
Mikolaj Sobanski <mikolajsobanski01@gmail.com>
2023-06-08 22:45:12 +0200

    dockerize

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Routes } from "react-router-dom";
 import LoginScreen from './screens/LoginScreen'
